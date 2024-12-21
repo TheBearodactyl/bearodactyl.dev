6256275a5f25bbc7f604ea8fe8a911eeaed312f9
The Bearodactyl <dabewodactowluwu@gmail.com>
2024-12-21 01:00:31 -0600

    add embed data :D

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site:name', content: 'The Motherfucking Bearodactyl' },
+        { property: 'og:description', content: 'rabies' },
+      ]
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],

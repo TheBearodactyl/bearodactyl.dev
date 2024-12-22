ba0ac663296b0f9f925ec6454e91061537df6eee
The Bearodactyl <dabewodactowluwu@gmail.com>
2024-12-21 21:32:49 -0600

    fix embed size

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <h1 class="animate-text">
       AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     </h1>
-    <iframe width="500" height="500" src="https://youtube.com/embed/ypHZ_iKBcoo?autoplay=1&loop=1"
+    <iframe width="500" height="280" src="https://youtube.com/embed/ypHZ_iKBcoo?autoplay=1&loop=1"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen

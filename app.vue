cc9cce143fe07e54db81db48dd66abd16823c28d
The Bearodactyl <dabewodactowluwu@gmail.com>
2024-12-21 21:17:28 -0600

    AAAAAAAAAAAAAAAAAAAAAA

--- app.vue
+++ app.vue
@@ -1,23 +1,44 @@
 <template>
   <div class="container">
-    <h1 class="animate-text-grow">
-      BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    <h1 class="animate-text">
+      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     </h1>
+    <audio controls autoplay preload="auto" playsinline :src="aaaaaaUrl" style="display: none;"></audio>
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+const aaaaaaUrl = 'https://www.myinstants.com/media/sounds/ahhhh-song.mp3'
+const audioPlayer = ref(null)
+
+
+onMounted(() => {
+  if (audioPlayer.value) {
+    audioPlayer.value.play()
+      .then(() => {
+        audioPlayer.value.muted = false
+      })
+      .catch("console.error")
+  }
+})
+</script>
 
 <style>
 @keyframes textGrow {
   from {
     font-size: 1rem;
+    color: black;
   }
   
   to {
-    font-size: 5000rem;
+    font-size: 500rem;
+    color: pink;
   }
 }
 
-.animate-text-grow {
+.animate-text {
   animation: textGrow 120s ease-in-out forwards;
 }
 </style>

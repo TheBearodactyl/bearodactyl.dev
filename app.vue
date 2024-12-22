dcf932ec37422ef8d757f4fbe112f30b79f34d78
The Bearodactyl <dabewodactowluwu@gmail.com>
2024-12-21 21:31:22 -0600

    AAAAAAAAAAAAAAAAAAAAAA

--- app.vue
+++ app.vue
@@ -3,26 +3,16 @@
     <h1 class="animate-text">
       AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     </h1>
-    <audio controls autoplay preload="auto" playsinline :src="aaaaaaUrl" loop></audio>
+    <iframe width="500" height="500" src="https://youtube.com/embed/ypHZ_iKBcoo?autoplay=1&loop=1"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    >
+    </iframe>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
-
-const aaaaaaUrl = 'https://www.myinstants.com/media/sounds/ahhhh-song.mp3'
-const audioPlayer = ref(null)
-
-
-onMounted(() => {
-  if (audioPlayer.value) {
-    audioPlayer.value.play()
-      .then(() => {
-        audioPlayer.value.muted = false
-      })
-      .catch("console.error")
-  }
-})
 </script>
 
 <style>
@@ -39,6 +29,6 @@ onMounted(() => {
 }
 
 .animate-text {
-  animation: textGrow 120s ease-in-out forwards;
+  animation: textGrow 3607s ease-in-out forwards;
 }
 </style>

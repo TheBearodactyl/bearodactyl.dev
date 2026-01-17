96e66b002e07333a013826f06101401e97f2c045
The Bearodactyl <dabewodactowluwu@gmail.com>
2026-01-17 16:24:41 -0600

    e

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use rocket::fs::FileServer;
-use std::path::Path;
 
 #[rocket::launch]
 fn launch() -> _ {
-    let pages_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("pages");
-    rocket::build().mount("/", FileServer::from(pages_path))
+    rocket::build().mount("/", FileServer::from("pages"))
 }
